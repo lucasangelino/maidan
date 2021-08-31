fe432925ec85e71216c115be3cf983ff278fd9af
Lucas Angelino <angelino.n.lucas@gmail.com>
2021-08-30 21:43:30 -0300

    MAI style navbar

--- components/core/navbar/index.js
+++ components/core/navbar/index.js
@@ -1,5 +1,17 @@
 import styles from "./navbar.module.css";
 export default function Navbar() {
-  const { navbar } = styles;
-  return <div className={navbar}>navbar</div>;
+  const { navbar, actions, searchbox, socialmedia, mediabutton } = styles;
+  return (
+    <div className={navbar}>
+      <div>logo</div>
+      <div className={actions}>
+        <input className={searchbox} type="text" placeholder="Search" />
+        <div className={socialmedia}>
+          <div className={mediabutton}>Face</div>
+          <div className={mediabutton}>Inta</div>
+          <div className={mediabutton}>You</div>
+        </div>
+      </div>
+    </div>
+  );
 }
