3d405bf8e1d890750796295271f1242af95f7ec0
Lucas Angelino <angelino.n.lucas@gmail.com>
2021-08-31 17:38:53 -0300

    MAI style articles

--- components/ui/articles/Article.js
+++ components/ui/articles/Article.js
@@ -1,11 +1,14 @@
 import styles from "./article.module.css";
 export default function Article({ title, content, readTime }) {
-  const { article, readtime } = styles;
+  const { article, readtime, logo, article_content } = styles;
   return (
     <div className={article}>
-      <p className={readtime}>Tiempo de lectura {readTime} aprox</p>
-      <h3>{title}</h3>
-      <p>{content}</p>
+      <div className={logo}>Logo</div>
+      <div className={article_content}>
+        <p className={readtime}>Tiempo de lectura {readTime} aprox</p>
+        <h3>{title}</h3>
+        <p>{content}</p>
+      </div>
     </div>
   );
 }
