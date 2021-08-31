19236282b98130f030578167228bac585b9cc7c8
Lucas Angelino <angelino.n.lucas@gmail.com>
2021-08-31 18:13:38 -0300

    MAI change media for mobile

---
+++ constants/content/index.js
@@ -0,0 +1,75 @@
+const filters = [
+  {
+    title: "JS",
+    style: "#fae207",
+  },
+  {
+    title: "React",
+    style: "#5ED3F3",
+  },
+  {
+    title: "Games",
+    style: "#AE5CF7",
+  },
+  {
+    title: "IA",
+    style: "#040F46",
+  },
+  {
+    title: "Misc",
+    style: "#ff7b00",
+  },
+  {
+    title: "Node",
+    style: "#7FC728",
+  },
+];
+
+const articles = [
+  {
+    id: 1,
+    title: "Tu primer App con ReactJs",
+    content: "Creat una aplicacion con React en menos de 10 minutos...",
+    readTime: "15 min",
+    topic: "#fae207",
+  },
+  {
+    id: 1,
+    title: "Crea un servidor ultrarapido con NodeJs",
+    content: "NodeJs nos permite crear servidores para nuestras aplica...",
+    readTime: "15 min",
+    topic: "#7FC728",
+  },
+  {
+    id: 1,
+    title: "5 Design Systems que deberias probar",
+    content: "Algunos basicos, otros extensos y poderosos. Prueba estos...",
+    readTime: "15 min",
+    topic: "#ff7b00",
+  },
+  {
+    id: 1,
+    title: "React - create-react-app vs webpack app",
+    content: "Pros y contras de crear apps con create-react-app vs webp...",
+    readTime: "15 min",
+    topic: "#fae207",
+  },
+  {
+    id: 1,
+    title: "El Juego de la Vida",
+    content:
+      "Un juego que esconde secretos del universo. El juego de 0 jugadores",
+    readTime: "20 min",
+    topic: "#AE5CF7",
+  },
+  {
+    id: 1,
+    title: "Github Copilot",
+    content:
+      "¿Inteligencia Artificial que programa? Skynet? Robots haciendo Robots?",
+    readTime: "15 min",
+    topic: "#040F46",
+  },
+];
+
+export { filters, articles };
