0479957d7284c3a930c1919d32f1028fa771cc2f
Lucas Angelino <angelino.n.lucas@gmail.com>
2021-09-01 18:37:33 -0300

    MAI make hover transparent

--- components/ui/cards/index.js
+++ components/ui/cards/index.js
@@ -1,10 +1,19 @@
+import Image from "next/image";
 import styles from "./cards.module.css";
+import recomImg from "../../../pages/public/recom_video1.jpg";
 
 export default function Cards() {
-  const { card } = styles;
+  const { card, img } = styles;
   return (
     <>
-      <div className={card}>Card1</div>
+      <div className={card}>
+        <Image
+          src={recomImg}
+          alt={"Recomendacion video"}
+          layout={"fill"}
+          className={img}
+        />
+      </div>
     </>
   );
 }
