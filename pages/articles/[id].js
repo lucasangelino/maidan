c16ff7f6bbba002aafa140f61b8bd50984ff4c92
Lucas Angelino <angelino.n.lucas@gmail.com>
2021-09-02 17:25:21 -0300

    MAI add div parent to image tag

--- pages/articles/[id].js
+++ pages/articles/[id].js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Link from "next/link";
+import Image from "next/image";
 import { useRouter } from "next/router";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
@@ -10,10 +11,11 @@ import style from "./articlepage.module.css";
 // import rehypeRaw from 'rehype-raw'
 
 export default function Article(props) {
-  const { article_content } = style;
+  const { article_content, image_cotainter } = style;
   const [article, setArticle] = useState(null);
-  const URL =
-    "https://gist.githubusercontent.com/lucasangelino/4630aec21dd5a6f7371b5be51d6e94a3/raw/12f0fb93f204b37571ec9b60e1d0828da969a168/article.md";
+  const ARTICLE_ID = `cc7575d896207997960cb5430a4ebbe4aa7b16c8`;
+  const URL = `
+    https://gist.githubusercontent.com/lucasangelino/4630aec21dd5a6f7371b5be51d6e94a3/raw/${ARTICLE_ID}/article.md`;
   const router = useRouter();
   if (router.isFallback) return "Loading...";
 
@@ -32,7 +34,25 @@ export default function Article(props) {
       <BackButton onclick={handleBackClick} />
       {article && (
         <div className={article_content}>
-          <ReactMarkdown children={article} remarkPlugins={[remarkGfm]} />
+          <ReactMarkdown
+            children={article}
+            components={{
+              img: ({ node, ...props }) => {
+                return (
+                  <div className={image_cotainter} {...props}>
+                    <Image
+                      src={
+                        "https://raw.githubusercontent.com/lucasangelino/maidan-img/main/imgs/react-toturial.png?token=AGX6FDQ5A7NUVPKKTYQCXITBHJSYA"
+                      }
+                      layout={`fill`}
+                      alt={`Image`}
+                    />
+                  </div>
+                );
+              },
+            }}
+            remarkPlugins={[remarkGfm]}
+          />
         </div>
       )}
     </>
